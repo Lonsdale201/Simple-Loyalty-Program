f3868a3bf992ebca0196be346abaa84a1ebe0a97
Lonsdale201 <lonsdale201@hotmail.com>
2024-11-18 11:39:38 +0100

    fix css

--- templates/notifications-loggedin.php
+++ templates/notifications-loggedin.php
@@ -1,10 +1,10 @@
 <?php
 
 if (!defined('ABSPATH')) {
-    exit; // Exit if accessed directly
+    exit; 
 }
 
 ?>
-<div class="hw-pilla-notification">
-    <span class="hw-pilla-notification-text"><?php echo wp_kses_post($message); ?></span>
+<div class="hw-loyalty-notification">
+    <span class="hw-loyalty-notification-text"><?php echo wp_kses_post($message); ?></span>
 </div>
--- templates/notifications-loggedout.php
+++ templates/notifications-loggedout.php
@@ -1,10 +1,10 @@
 <?php
 
 if (!defined('ABSPATH')) {
-    exit; // Exit if accessed directly
+    exit; 
 }
 
 ?>
-<div class="hw-pilla-notification-logged--out">
-    <span class="hw-pilla-notification-text--out"><?php echo wp_kses_post($message); ?></span>
+<div class="hw-loyalty-notification-logged--out">
+    <span class="hw-loyalty-notification-text--out"><?php echo wp_kses_post($message); ?></span>
 </div>
